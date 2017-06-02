67ea85ed7be2fa9347bd412053bc4e67749825f8
Brian <ssy880106@gmail.com>
2017-06-02 17:44:44 +0900

    Add files via upload
    Created a CSS page, divided the header into a separate page, built an English page.

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
+<!-- header -->
+<?php include 'section/header.php'; ?>
+    
     <body>
         <header>
             <h1>Something Great shall Rise!</h1>
@@ -18,5 +10,7 @@ and open the template in the editor.
         <a href="portfolio.php">portfolio (what I have done)</a>
         <br><br>
         <a href="blog.php">blog (what I'm currently doing/learning)</a>
+        <br><br>
+        <a href="english.php">English Site (information on my English class)</a>
     </body>
 </html>
