3aee3d8ea567511e6685ac2135541d71fefe2763
Brian <ssy880106@gmail.com>
2017-06-03 15:15:49 +0900

    bootstrap landing index page
    Used a bootstrap landing page template for the index. Will try and implement other Bootstrap-like UI frameworks into the other pages. But needed something right away.

---
+++ nav/nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar">Portfolio</span>
+        <span class="icon-bar">Eng </span>
+        <span class="icon-bar">Blog</span>
+      </button>
+      <a class="navbar-brand" href="index.php">Brian (신수용)</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="portfolio.php">Portfolio</a></li>
+        <li><a href="english.php">Eng Tutoring</a></li>
+        <li><a href="blog.php">Blog</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
